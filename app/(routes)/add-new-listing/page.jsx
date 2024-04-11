5007ddf3a88ed2cae833e89a5247c916b5615185
PIN-CHIEN TSENG <pin-chientseng@PIN-CHIENs-MacBook-Pro.local>
2024-04-11 21:33:07 +1000

    fitst commit

---
+++ app/(routes)/add-new-listing/page.jsx
@@ -0,0 +1,58 @@
+'use client'
+
+import GoogleAddressSearch from '@/app/_components/GoogleAddressSearch'
+import { Button } from '@/components/ui/button'
+import { supabase } from '@/utils/supabase/client';
+import { useUser } from '@clerk/nextjs';
+import { Loader } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { toast } from 'sonner';
+
+function AddNewListing() {
+    const [selectedAddress,setSelectedAddress] = useState();
+    const [coordinates, setCoordinates] = useState();
+    const {user} = useUser();
+    const router= useRouter();
+    const [loading, setLoading] = useState(false);
+    const nextHandler = async() => {
+        setLoading(true)
+        const { data, error } = await supabase
+        .from('listing')
+        .insert([
+        { address: selectedAddress.label, coordinates: coordinates, createdBy: user?.primaryEmailAddress.emailAddress},
+        ])
+        .select()
+        
+        if(data){
+            setLoading(false)
+            //console.log("New Data added", data)
+            toast("New Address added for listing")
+            //console.log("New Data added", data[0].id)
+            router.replace('/edit-listing/'+data[0].id)
+        } else {
+            setLoading(false)
+            console.log("Error")
+            toast('Something has error')
+        }
+    }
+  return (
+    <div className='mt-10 md:mx-56 lg:mx-80'>
+        <div className='p-10 flex flex-col gap-5 items-center justify-center'>
+            <h2 className='font-bold text-2xl'>Add New Listing</h2>
+            <div className='p-10 rounded-lg border w-full shadow-md flex flex-col gap-5'>
+                <h2 className='text-gray-500'>Enter Address which your want to list</h2>
+                <GoogleAddressSearch 
+                    selectedAddress={(value)=> setSelectedAddress(value)}
+                    setCoordinates={(value)=> setCoordinates(value)}
+                />
+                <Button onClick={nextHandler} disabled={!selectedAddress || !coordinates || loading}>
+                    {loading? <Loader className='animate-spin' /> : 'Next'}
+                    </Button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddNewListing
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Provider from "./Provider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +13,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>
+        {children}
+        <Toaster />
+        </Provider>
+        </body>
     </html>
+    </ClerkProvider>
   );
 }
